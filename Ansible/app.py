18b8ecf5b068477bf5032876bf4f2e691a5e9396
Admin <sergz99@gmail.com>
2020-11-28 19:46:38 +0500

    1

--- Ansible/app.py
+++ Ansible/app.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python3
 
 import datetime
+import os
 
 def do_magic():
-    now=datetime.datetime.now()
-    return "Hello!{0}".format(now)
+  now = datetime.datetime.now()
+  return "Hello! {0}".format(now)
 
-if __name__ == '__main__':
-    print(do_magic())
+def application(env, start_response):
+  start_response('200 OK', [('Content-Type','text/html')])
+  return [do_magic().encode()]
+
+if __name__ == "__main__":
+  if 'REQUEST_URI' in os.environ:
+    print("Content-type: text/html\n\n")
+  print(do_magic())
